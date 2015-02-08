ee0bfb04a2464efc628f5d2aa96ab4fa1a07e72d
mattyangk <kany@andrew.cmu.edu>
2015-02-07 19:53:01 -0500

    json jar

--- WebserviceMovie/src/model/TMDBRetriever.java
+++ WebserviceMovie/src/model/TMDBRetriever.java
@@ -1,29 +1,42 @@
 package model;
 
+import java.io.InputStream;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+
 public class TMDBRetriever {
 	
-	private HttpsURLConnection connection;
+	HttpClient httpClient;
 	
 	private static final String key = "e3d6e2a242e56f63200526c0c531a92c";
 	
 	private static final String urlString = "https://api.themoviedb.org/3/";
 	
-	public TMDBRetriever() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	public List<MovieBean> getMoviesByTitle(String title) {
+		httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet(urlString + "search/movie?api_key="+key);
 		
+		HttpResponse response = httpClient.execute(httpGet);
+		HttpEntity entity = response.getEntity();
+		
+		if (entity != null) {
+			InputStream instream = entity.getContent();
+			
+		}
+		return null;
 	}
 	
 	public List<MovieBean> getTopMovies() {
 		
 	}
 	
-	public 
 
 }
