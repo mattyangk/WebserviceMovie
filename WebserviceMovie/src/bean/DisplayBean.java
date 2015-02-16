17267234d76c0dabaed5c02ca15b2ea159ccde25
CdvSteven <shidiweiwilly@gmail.com>
2015-02-15 22:13:37 -0500

    DisplayBean

---
+++ WebserviceMovie/src/bean/DisplayBean.java
@@ -0,0 +1,55 @@
+package bean;
+
+import java.util.Date;
+
+public class DisplayBean {
+	private String text;
+	private String user_name;
+	private String photo_url;
+	private String profile_url;
+	private Date date;
+	private String source;
+	
+	
+	
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPhoto_url() {
+		return photo_url;
+	}
+	public void setPhoto_url(String photo_url) {
+		this.photo_url = photo_url;
+	}
+	public String getProfile_url() {
+		return profile_url;
+	}
+	public void setProfile_url(String profile_url) {
+		this.profile_url = profile_url;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	
+
+}
