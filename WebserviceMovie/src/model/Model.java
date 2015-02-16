75063069cb6f95abb6775341bb1cadcc8823c0af
Anjal <anjalpatni@yahoo.co.in>
2015-02-15 21:36:33 -0500

    twitter in model

--- WebserviceMovie/src/model/Model.java
+++ WebserviceMovie/src/model/Model.java
@@ -30,7 +30,7 @@ public class Model {
 			postDAO = new PostDAO(pool, "Post");
 			commentDAO = new CommentDAO(pool,"Comment");
 			tmdbRetriever = new TMDBRetriever();
-
+			twitterRetriever = new TwitterRetriever();
 			
 			
 			//if (userDAO.getCount() == 0){
