aed8046478feca68c5ce8e93ac9865252e5dcb40
Anjal <anjalpatni@yahoo.co.in>
2015-02-18 03:22:37 -0500

    posting

--- WebserviceMovie/src/controller/PostAction.java
+++ WebserviceMovie/src/controller/PostAction.java
@@ -48,80 +48,84 @@ public class PostAction extends Action {
 
 		HttpSession session = request.getSession();
 
-		if (isRepost!=null&&isRepost.equals("repost")) {
+		try{
+			if (isRepost!=null&&isRepost.equals("repost")) {
 
-			if (source.equals("Flickr")) {
-				if (session.getAttribute("FaccessToken") == null) {
-					String url= "startFlickerLogin.do?comment=" + Encoder.encode(comment)
-							+ "&ori_poster=" + Encoder.encode(ori_poster) + "&ori_text="
-							+ Encoder.encode(ori_text) + "&imagePath=" + Encoder.encode(imagePath)
-							+ "&category=" + Encoder.encode(category) + "&isRepost=" + Encoder.encode(isRepost)
-							+ "&user_id=" + user.getUser_id() + "&source=" + Encoder.encode(source) + "&photoID=" + photoID;
-					System.out.println(url);
-					return url;
-				} else {
-					Token accessToken=(Token)session.getAttribute("FaccessToken");
-					System.out.println("Token is:" + accessToken.getToken());
-					OAuthRequest flickrRequest=new OAuthRequest(Verb.POST,PROTECTED_RESOURCE_URL);
-					flickrRequest.addQuerystringParameter("method", "flickr.photos.comments.addComment");
-					flickrRequest.addBodyParameter("photo_id", photoID);
-					flickrRequest.addBodyParameter("comment_text", comment);
-					OAuthService Flickerservice = (OAuthService) session.getAttribute("Fservice");
-					Flickerservice.signRequest(accessToken, flickrRequest);
-					Response flickrResponse = flickrRequest.send();
-					System.out.println(flickrResponse.getBody());	
+				if (source.equals("Flickr")) {
+					if (session.getAttribute("FaccessToken") == null) {
+						String url= "startFlickerLogin.do?comment=" + Encoder.encode(comment)
+								+ "&ori_poster=" + Encoder.encode(ori_poster) + "&ori_text="
+								+ Encoder.encode(ori_text) + "&imagePath=" + Encoder.encode(imagePath)
+								+ "&category=" + Encoder.encode(category) + "&isRepost=" + Encoder.encode(isRepost)
+								+ "&user_id=" + user.getUser_id() + "&source=" + Encoder.encode(source) + "&photoID=" + photoID;
+						System.out.println(url);
+						return url;
+					} else {
+						Token accessToken=(Token)session.getAttribute("FaccessToken");
+						System.out.println("Token is:" + accessToken.getToken());
+						OAuthRequest flickrRequest=new OAuthRequest(Verb.POST,PROTECTED_RESOURCE_URL);
+						flickrRequest.addQuerystringParameter("method", "flickr.photos.comments.addComment");
+						flickrRequest.addBodyParameter("photo_id", photoID);
+						flickrRequest.addBodyParameter("comment_text", comment);
+						OAuthService Flickerservice = (OAuthService) session.getAttribute("Fservice");
+						Flickerservice.signRequest(accessToken, flickrRequest);
+						Response flickrResponse = flickrRequest.send();
+						System.out.println(flickrResponse.getBody());	
 
-					// post this back to flickr
-				}
-			} 
-			// twitter
-			else {
-				if (session.getAttribute("loggedTwitter") == null) {
-					String url= "startTwitterLogin.do?comment=" + Encoder.encode(comment)
-							+ "&ori_poster=" + Encoder.encode(ori_poster) + "&ori_text="
-							+ Encoder.encode(ori_text) + "&imagePath=" + Encoder.encode(imagePath)
-							+ "&category=" + Encoder.encode(category) + "&isRepost=" + Encoder.encode(isRepost)
-							+ "&user_id=" + user.getUser_id() + "&source=" + Encoder.encode(source);
-					System.out.println(url);
-					return url;
-				} else {
+						// post this back to flickr
+					}
+				} 
+				// twitter
+				else {
+					if (session.getAttribute("loggedTwitter") == null) {
+						String url= "startTwitterLogin.do?comment=" + Encoder.encode(comment)
+								+ "&ori_poster=" + Encoder.encode(ori_poster) + "&ori_text="
+								+ Encoder.encode(ori_text) + "&imagePath=" + Encoder.encode(imagePath)
+								+ "&category=" + Encoder.encode(category) + "&isRepost=" + Encoder.encode(isRepost)
+								+ "&user_id=" + user.getUser_id() + "&source=" + Encoder.encode(source);
+						System.out.println(url);
+						return url;
+					} else {
 
-					try{
-						OAuthService service= (OAuthService) session.getAttribute("service");
-						Token accessToken = (Token) session.getAttribute("accessToken");
-						String tweet = URLEncoder.encode(comment,"UTF-8");
-						String urlTweet="https://api.twitter.com/1.1/statuses/update.json?status="+tweet;
-						System.out.println("request: "+urlTweet);
-						OAuthRequest request2 = new OAuthRequest(Verb.POST, urlTweet);
-						service.signRequest(accessToken, request2);
-						System.out.println("REQUEST: " + request2.getUrl());
-						Response response2 = request2.send();
-						System.out.println("Response body:"+response2.getBody());
-					} catch(Exception e){
-						e.getMessage();
+						try{
+							OAuthService service= (OAuthService) session.getAttribute("service");
+							Token accessToken = (Token) session.getAttribute("accessToken");
+							String tweet = URLEncoder.encode(comment,"UTF-8");
+							String urlTweet="https://api.twitter.com/1.1/statuses/update.json?status="+tweet;
+							System.out.println("request: "+urlTweet);
+							OAuthRequest request2 = new OAuthRequest(Verb.POST, urlTweet);
+							service.signRequest(accessToken, request2);
+							System.out.println("REQUEST: " + request2.getUrl());
+							Response response2 = request2.send();
+							System.out.println("Response body:"+response2.getBody());
+						} catch(Exception e){
+							e.getMessage();
+						}
 					}
 				}
+
 			}
 
+			PostBean post = new PostBean();
+			post.setCategory(category);
+			post.setContent(comment + "//@" + ori_poster + " " + ori_text);
+			post.setImagePath(imagePath);
+			post.setUser_id(user.getUser_id());
+
+			System.out.println("is repost?" + isRepost);
+
+			try {
+				postDAO.create(post);
+			} catch (RollbackException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			return "homePage.do";
+		}catch(Exception e){
+			System.out.println("Exception :"+e);
+			return "homePage.do";
 		}
-
-		PostBean post = new PostBean();
-		post.setCategory(category);
-		post.setContent(comment + "//@" + ori_poster + " " + ori_text);
-		post.setImagePath(imagePath);
-		post.setUser_id(user.getUser_id());
-
-		System.out.println("is repost?" + isRepost);
-
-		try {
-			postDAO.create(post);
-		} catch (RollbackException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return "homePage.do";
-
 	}
 
 }
--- WebserviceMovie/src/controller/PostNewAction.java
+++ WebserviceMovie/src/controller/PostNewAction.java
@@ -36,7 +36,7 @@ import model.UploadPhoto;
 public class PostNewAction extends Action {
 
 	PostDAO postDAO;
-	
+
 	public PostNewAction(Model model) {
 		postDAO = model.getPostDAO();
 	}
@@ -55,126 +55,149 @@ public class PostNewAction extends Action {
 
 		File file = null;
 		String fileName = "";
-		try {
-			Part filePart = request.getPart("upload");
-			fileName = getFileName(filePart);
-			System.out.println("file name " + fileName);
-			InputStream fileContent = filePart.getInputStream();
-			System.out.println("real path: " + request.getServletContext().getRealPath(""));
-			file = new File(request.getServletContext().getRealPath("") +"/images/"+ fileName);
-			
-			if (!file.exists()) {
-				file.createNewFile();
+		try{
+			try {
+				Part filePart = request.getPart("upload");
+				fileName = getFileName(filePart);
+				System.out.println("file name " + fileName);
+				InputStream fileContent = filePart.getInputStream();
+				System.out.println("real path: " + request.getServletContext().getRealPath(""));
+				file = new File(request.getServletContext().getRealPath("") +"/images/"+ fileName);
+
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+				System.out.println(file.getAbsolutePath());
+				OutputStream output = new FileOutputStream(file);
+				byte[] buffer = new byte[1024];
+				int read = 0;
+				while ((read = fileContent.read(buffer)) != -1) {
+					output.write(buffer, 0, read);
+				}
+				fileContent.close();
+				output.close();
+
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			System.out.println(file.getAbsolutePath());
-			OutputStream output = new FileOutputStream(file);
-			byte[] buffer = new byte[1024];
-			int read = 0;
-			while ((read = fileContent.read(buffer)) != -1) {
-				output.write(buffer, 0, read);
+
+			HttpSession session = request.getSession();
+
+			UserBean user = (UserBean)session.getAttribute("user");
+			PostBean post = new PostBean();
+			post.setCategory("other");
+			post.setContent(content);
+			post.setImagePath("images/"+ fileName);
+			post.setPostDate(new Date());
+			post.setUser_id(user.getUser_id());
+
+			try {
+				postDAO.create(post);
+			} catch (RollbackException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-			fileContent.close();
-			output.close();
-			
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ServletException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		HttpSession session = request.getSession();
-		
-		UserBean user = (UserBean)session.getAttribute("user");
-		PostBean post = new PostBean();
-		post.setCategory("other");
-		post.setContent(content);
-		post.setImagePath("images/"+ fileName);
-		post.setPostDate(new Date());
-		post.setUser_id(user.getUser_id());
-		
-		try {
-			postDAO.create(post);
-		} catch (RollbackException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		if (reposts == null) {
+
+			if (reposts == null) {
+				return "allPostsPage.do";
+			}
+
+
+
+			if(reposts.length==2){
+				try {
+
+					UploadPhoto.uploadFlicker("-u yuexuanl -s "+content+ " "+file.getAbsolutePath()+" -apiKey 992c097a02257b03f4e1b7fac88fab5e -secret 85aeacf925c41b39");
+
+					Token requestToken = (Token) session.getAttribute("requestToken");
+					OAuthService service = (OAuthService) session.getAttribute("service");
+
+					Verifier verifier = new Verifier((String) session.getAttribute("oauth_verifier"));
+
+					Token accessToken = service.getAccessToken(requestToken, verifier);
+
+					String access_token = accessToken.getToken();
+					String access_token_secret = accessToken.getSecret();
+
+
+					System.out.println("access_token : "+access_token);
+					System.out.println("access_token_secret : "+access_token_secret);
+
+					Twitter.updateStatusWithMedia(access_token, access_token_secret, content, new File(file.getAbsolutePath()));
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			else if(reposts[0].equals("flickr")){
+				try {
+					UploadPhoto.uploadFlicker("-u yuexuanl -s "+content+ " "+file.getAbsolutePath()+" -apiKey 992c097a02257b03f4e1b7fac88fab5e -secret 85aeacf925c41b39");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else if(reposts[0].equals("twitter")){
+				try {
+
+					Token requestToken = (Token) session.getAttribute("requestToken");
+					OAuthService service = (OAuthService) session.getAttribute("service");
+
+					Verifier verifier = new Verifier((String) session.getAttribute("oauth_verifier"));
+
+					Token accessToken = service.getAccessToken(requestToken, verifier);
+
+					String access_token = accessToken.getToken();
+					String access_token_secret = accessToken.getSecret();
+
+
+					System.out.println("access_token : "+access_token);
+					System.out.println("access_token_secret : "+access_token_secret);
+
+					Twitter.updateStatusWithMedia(access_token, access_token_secret, content, new File(file.getAbsolutePath()));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+
+
+
+
+			//		System.out.println("is upload? "
+			//				+ ServletFileUpload.isMultipartContent(request));
+			//
+			//		ServletFileUpload upload = new ServletFileUpload();
+			//		FileItemIterator it;
+			//		try {
+			//			it = upload.getItemIterator(request);
+			//			while (it.hasNext()) {
+			//				FileItemStream item = it.next();
+			//				String name = item.getFieldName();
+			//				System.out.println("name: " + name);
+			//			}
+			//		} catch (FileUploadException | IOException e) {
+			//			// TODO Auto-generated catch block
+			//			e.printStackTrace();
+			//		}
+
+			return "allPostsPage.do";
+
+		}catch(Exception e){
+			System.out.println("exception :"+e.getMessage());
 			return "allPostsPage.do";
 		}
-
-		Token requestToken = (Token) session.getAttribute("requestToken");
-		OAuthService service = (OAuthService) session.getAttribute("service");
-		
-		Verifier verifier = new Verifier((String) session.getAttribute("oauth_verifier"));
-		
-		Token accessToken = service.getAccessToken(requestToken, verifier);
-		
-		String access_token = accessToken.getToken();
-		String access_token_secret = accessToken.getSecret();
-		
-		
-		System.out.println("access_token : "+access_token);
-		System.out.println("access_token_secret : "+access_token_secret);
-		
-		if(reposts.length==2){
-			try {
-				
-				UploadPhoto.uploadFlicker("-u yuexuanl -s "+content+ " "+file.getAbsolutePath()+" -apiKey 992c097a02257b03f4e1b7fac88fab5e -secret 85aeacf925c41b39");
-				Twitter.updateStatusWithMedia(access_token, access_token_secret, content, new File(file.getAbsolutePath()));
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		else if(reposts[0].equals("flickr")){
-			try {
-				UploadPhoto.uploadFlicker("-u yuexuanl -s "+content+ " "+file.getAbsolutePath()+" -apiKey 992c097a02257b03f4e1b7fac88fab5e -secret 85aeacf925c41b39");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		else if(reposts[0].equals("twitter")){
-			try {
-				Twitter.updateStatusWithMedia(access_token, access_token_secret, content, new File(file.getAbsolutePath()));
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-  
-
-		
-		
-
-//		System.out.println("is upload? "
-//				+ ServletFileUpload.isMultipartContent(request));
-//
-//		ServletFileUpload upload = new ServletFileUpload();
-//		FileItemIterator it;
-//		try {
-//			it = upload.getItemIterator(request);
-//			while (it.hasNext()) {
-//				FileItemStream item = it.next();
-//				String name = item.getFieldName();
-//				System.out.println("name: " + name);
-//			}
-//		} catch (FileUploadException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
-		return "allPostsPage.do";
 	}
-
 	/**
 	 * Utility method to get file name from HTTP header content-disposition
 	 */
